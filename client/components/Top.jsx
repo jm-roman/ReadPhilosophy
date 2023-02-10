5ffe3de12874166c837aeb512ae7ff716f2eca0a
jm-roman <johnmroman33@gmail.com>
2023-02-10 13:44:48 -0500

    final presentation version

--- client/components/Top.jsx
+++ client/components/Top.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Top = (props) => {
   return (
     <div className='top-box'>
-      <div className='top'>Kant: GMM, Preface</div>
+      <div className='top'>
+        Kant: Groundwork for the Metaphysics of Morals, Preface
+      </div>
     </div>
   );
 };
